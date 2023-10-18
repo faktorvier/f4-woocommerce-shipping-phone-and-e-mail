05d9e4c7fc9e37a48f8cc2f5581f57aa34c0036f
Stephan Langenegger <stephan@faktorvier.ch>
2023-10-18 11:45:04 +0200

    Declare custom_order_tables and product_block_editor compatibilities

--- f4-woocommerce-shipping-phone-and-e-mail.php
+++ f4-woocommerce-shipping-phone-and-e-mail.php
@@ -4,15 +4,15 @@
 Plugin Name: F4 Shipping Phone and E-Mail for WooCommerce
 Plugin URI: https://github.com/faktorvier/f4-woocommerce-shipping-phone-and-e-mail
 Description: Adds fields for e-mail and/or telephone to the WooCommerce shipping address.
-Version: 1.0.17
+Version: 1.0.18
 Author: FAKTOR VIER
 Author URI: https://www.f4dev.ch
 License: GPLv2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: f4-woocommerce-shipping-phone-and-e-mail
 Domain Path: /languages/
-WC requires at least: 3.0
-WC tested up to: 7.1
+WC requires at least: 8.0
+WC tested up to: 8.2
 
 This plugin is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -31,7 +31,7 @@ along with this plugin. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 
 if(!defined('ABSPATH')) exit; // don't access directly
 
-define('F4_WCSPE_VERSION', '1.0.17');
+define('F4_WCSPE_VERSION', '1.0.18');
 
 define('F4_WCSPE_SLUG', 'f4-woocommerce-shipping-phone-and-e-mail');
 define('F4_WCSPE_MAIN_FILE', __FILE__);
